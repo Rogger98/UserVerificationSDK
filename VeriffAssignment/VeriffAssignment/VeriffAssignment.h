b1fc22993b3e3825b9811a25a4c2aba49d2a74d5
Sagar Prajapati <psagc@allstate.com>
2022-03-30 19:08:20 +0530

    Project Created

---
+++ VeriffAssignment/VeriffAssignment/VeriffAssignment.h
@@ -0,0 +1,18 @@
+//
+//  VeriffAssignment.h
+//  VeriffAssignment
+//
+//  Created by psagc on 30/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VeriffAssignment.
+FOUNDATION_EXPORT double VeriffAssignmentVersionNumber;
+
+//! Project version string for VeriffAssignment.
+FOUNDATION_EXPORT const unsigned char VeriffAssignmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VeriffAssignment/PublicHeader.h>
+
+
